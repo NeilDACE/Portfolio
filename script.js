7fc4514ae1608190db13e6f6343dc7bbab35b412
NeilDACE <Nil.vollhardt@gmail.com>
2026-05-12 13:49:42 +0200

    Add burger menu SVGs and enhance menu toggle functionality; improve accessibility and animations

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const menuOverlay = document.getElementById("full-screen-menu");
 menuToggle.addEventListener("click", () => {
   menuOverlay.classList.toggle("is-open");
   document.body.classList.toggle("no-scroll");
+  if (menuToggle.classList.contains("is-active")) {
+    menuToggle.classList.remove("is-active");
+    menuToggle.classList.add("is-closed");
+    menuToggle.setAttribute("aria-expanded", "false");
+  } else {
+    menuToggle.classList.remove("is-closed");
+    menuToggle.classList.add("is-active");
+    menuToggle.setAttribute("aria-expanded", "true");
+  }
 });
 
 const menuLinks = document.querySelectorAll(".menu-link");
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
+    menuToggle.classList.remove("is-active");
+    menuToggle.classList.add("is-closed");
     menuOverlay.classList.remove("is-open");
     document.body.classList.remove("no-scroll");
+    menuToggle.setAttribute("aria-expanded", "false");
   });
 });
